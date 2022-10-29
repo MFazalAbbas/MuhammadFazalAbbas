00e56befdde86c74fc55f4472a5910a9f47e3a01
mutaha-khan <94639595+mutaha-khan@users.noreply.github.com>
2022-10-29 12:49:28 +0500

    js arrays, objects and loop functions

---
+++ 29-10-22/array.js
@@ -0,0 +1,16 @@
+const array = [4, 9, 16, 25]
+
+// let arr = array.find((ele) => {
+//   if (ele == '6') return ele
+// })
+// console.log(arr)
+
+// const ages = [32, 33, 16, 40]
+// const result = ages.filter(checkAdult)
+
+// function checkAdult(age) {
+//   return age >= 18
+// }
+
+let ele = array.map(Math.sqrt)
+console.log(ele)
---
+++ 29-10-22/index.js
@@ -0,0 +1,17 @@
+const Employee = {
+  EmployeeID: 'faizan',
+  EmployeeName: '',
+  EmployeeCompany: 'MERN',
+  EmployeeCompanyAddress: {
+    city: '',
+    country: '',
+  },
+  EmployeeAddress: {
+    city: '',
+    country: '',
+  },
+}
+console.log(Employee.EmployeeCompanyAddress.city)
+console.log(Employee.EmployeeAddress.city)
+
+// console.log(student['address']['city'])
